e2a93e71b6d1328596ce8fcbcf7e03f48ac7ee9e
joaodiniz01 <joao.diniz@tecnico.ulisboa.pt>
2021-04-04 23:26:53 +0100

    report novo

--- t2/mat/AnaliseTeorica.m
+++ t2/mat/AnaliseTeorica.m
@@ -114,7 +114,7 @@ V1
 V2 = solutionnodes(1)
 V3 = solutionnodes(2)
 V5 = solutionnodes(3)
-V6 = solutionnodes(4)
+V6_a = solutionnodes(4)
 V7 = solutionnodes(5)
 V8 = solutionnodes(6)
 Id = solutionnodes(7)
@@ -127,7 +127,7 @@ I1 = (V2-V1)/R1
 I2 = Ib
 I3 = (V5-V2)/R3
 I4 = V5/R4
-I5 = (V6-V5)/R5
+I5 = (V6_a-V5)/R5
 I6 = Id
 I7 = Id
 Is = I1
@@ -139,7 +139,7 @@ fprintf(fid, "$V_1$ & %e \\\\ \\hline \n", V1);
 fprintf(fid, "$V_2$ & %e \\\\ \\hline \n", V2);
 fprintf(fid, "$V_3$ & %e \\\\ \\hline \n", V3);
 fprintf(fid, "$V_5$ & %e \\\\ \\hline \n", V5);
-fprintf(fid, "$V_6$ & %e \\\\ \\hline \n", V6);
+fprintf(fid, "$V_6$ & %e \\\\ \\hline \n", V6_a);
 fprintf(fid, "$V_7$ & %e \\\\ \\hline \n", V7);
 fprintf(fid, "$V_8$ & %e \\\\ \\hline \n", V8);
 fprintf(fid, "$I_1$ & %e \\\\ \\hline \n", I1);
@@ -173,7 +173,7 @@ C = (str2num(sprintf('%.11f', data(9))))
 Vs = 0
 
 %da alinea anterior temos que 
-Vx = V6-V8
+Vx = V6_a-V8
 V1 = Vs
 
 
@@ -248,8 +248,8 @@ fclose (fid);
 
 A = Vx-V8
 
-t = 0:0.1:20
-V6n = A*exp(-t/tau)
+t = 0:0.1:20;
+V6n = A*exp(-t/tau);
 
 hf = figure ();
 plot(t, V6n)
@@ -270,35 +270,37 @@ data = data';
 fclose(dados);
 
 C = (str2num(sprintf('%.11f', data(9))))*(10^-6)
+Req = Req*1000
+
+#vc = -i/(1 + i*2*pi*1000*Req*C)
+
+Z = 1/(i*2*pi*1000*C)
 
 Vs = 1
-v_s = Vs*exp(-i*(pi/2))
+v_s = -i
 
 V1 = v_s
 
-J = [ -1/R1-1/R2-1/R3   , 1/R2    , 1/R3    , 0 , 0 , 0 , 0     , 0     , 0   ;...
-      1/R3    , 0    , -1/R3-1/R5-1/R4   , 1/R5 ,0     , 0     , 1    , 0     , 0       ;...
-      0   , 0    , 1/R5  , -1/R5    , 0       , 0     , 0     , -1     , -1    ;...
-      0    , 0    , 0   ,    0       , 1/R7     , -1/R7    , -1  , 0     , 1  ;...
-      0    , 0    , 1    , 0   , 0      , -1    , -Kd    , 0     , 0     ;...
-      1/R1    , 0   , 1/R4   , 0   , 0       , 0     , -1     , 0     , 0       ;...
-      1/R2    , -1/R2   , 0    , 0   , 0       , 0     , 0     , 1    , 0     ;...
-      1   , 0    , -1    , 0   , 0       , 0     , 0     , -1/Kb     , 0      ;...
-      0    , 0    , 0    , 0   ,  1/R6      , 0     , 1    , 0     , 0    ];
+J = [ 1/R1+1/R2+1/R3   , -1/R2    , -1/R3    , 0 , 0 , 0 , 0    ;...
+        0    , 0    , 0   ,   -1      , 0    , 1    , 1      ;...
+        0    , 0    ,   0 , 0   , 1/R6+1/R7      , -1/R7    , 0     ;...
+        -1/R1    , 0   , -1/R4   , 0   , -1/R6       , 0     , 0      ;...
+        -1/R2-Kb    , 1/R2   , Kb   , 0   , 0       , 0     , 0      ;...
+        Kb   , 0    , -Kb-1/R5    , 1/R5+1/Z  , 0       ,-1/Z     , 0   ;...
+        0    , 0    , 1   , 0   , Kd/R6     , -1     , 0   ];
+            
       
-L = [ -V1/R1; 0; 0; 0; 0; V1/R1; 0; 0; 0];
+L = [ V1/R1; 0; 0; -V1/R1; 0;0;0];
 
 solutionnodes = J\L;
-V1
+
 V2 = solutionnodes(1)
 V3 = solutionnodes(2)
 V5 = solutionnodes(3)
 V6_ph = solutionnodes(4)
 V7 = solutionnodes(5)
 V8 = solutionnodes(6)
-Id = solutionnodes(7)
-Ib = solutionnodes(8)
-Ic = solutionnodes(9)
+vc = solutionnodes(7)
 
 % Table
 fid = fopen ("../doc/node4.tex", "w");
@@ -306,20 +308,19 @@ fprintf(fid, "$V_1$ & %e \\\\ \\hline \n", V1);
 fprintf(fid, "$V_2$ & %e \\\\ \\hline \n", V2);
 fprintf(fid, "$V_3$ & %e \\\\ \\hline \n", V3);
 fprintf(fid, "$V_5$ & %e \\\\ \\hline \n", V5);
-fprintf(fid, "$V_6$ & %e \\\\ \\hline \n", V6);
+fprintf(fid, "$V_6$ & %e \\\\ \\hline \n", V6_ph);
 fprintf(fid, "$V_7$ & %e \\\\ \\hline \n", V7);
 fprintf(fid, "$V_8$ & %e \\\\ \n", V8);
 fclose (fid);
 
 V6_modul = abs(V6_ph)
-Req = Req*1000
 fase6 = 0 + atan(2*pi*1000*Req*C)
 
 t =  0:0.1:20;
 V6f = V6_modul*cos((t/1000)*2*1000*pi - fase6);
 
 hf1 = figure ();
-plot(t, V6f)
+plot(t, -V6f)
 xlabel ("t (ms)")
 ylabel ("V6f (V)")
 title ("Forced Solution V6f(t)")
@@ -341,14 +342,73 @@ Vs = str2num(sprintf('%.11f', data(8)));
 
 % t =[0,20]
 t1 =  0:0.1:20;
-vs_t1 = sin(2*pi*1000*(t1/1000))
-V6_t1 = V6n + V6f
+vs_t1 = sin(2*pi*1000*(t1/1000));
+V6_t1 = V6n + V6f;
 % t<0
-V6 = 5.702372676500307
-t2 = -5:0.1:0
-vs_t2 = Vs
-V6_t2 = V6
+t2 = -5:0.1:0;
+vs_t2 = Vs;
+V6_t2 = V6_a;
 
 hf2 = figure ();
 plot(t1, V6_t1, 'b',t1 ,vs_t1, 'r', t2, V6_t2, 'b', t2, vs_t2, 'r')
+xlabel ("t (ms)")
+ylabel ("Voltage (V)")
 print (hf2, "alinea5.eps", "-depsc");
+
+
+########################################
+# Alinea 6
+########################################
+
+f = logspace(-1, 6, 70)
+
+Z = 1./(i*2*pi*f*C)
+
+Vs = 1
+vs_f = -i
+##
+V1 = vs_f
+
+for k = 1:70
+
+P = [ 1/R1+1/R2+1/R3   , -1/R2    , -1/R3    , 0 , 0 , 0 , 0    ;...
+        0    , 0    , 0   ,   -1      , 0    , 1    , 1      ;...
+        0    , 0    ,   0 , 0   , 1/R6+1/R7      , -1/R7    , 0     ;...
+        -1/R1    , 0   , -1/R4   , 0   , -1/R6       , 0     , 0      ;...
+        -1/R2-Kb    , 1/R2   , Kb   , 0   , 0       , 0     , 0      ;...
+        Kb   , 0    , -Kb-1/R5    , 1/R5+1/Z(k)  , 0       ,-1/Z(k)     , 0   ;...
+        0    , 0    , 1   , 0   , Kd/R6     , -1     , 0   ];
+            
+      
+Q = [ V1/R1; 0; 0; -V1/R1; 0;0;0];
+
+solutionnodes = P\Q;
+
+V2 = solutionnodes(1)
+V3 = solutionnodes(2)
+V5 = solutionnodes(3)
+V6_f(k) = solutionnodes(4)
+V7 = solutionnodes(5)
+V8 = solutionnodes(6)
+vc_f(k) = solutionnodes(7)
+endfor
+##
+##
+##
+
+figure; hold on
+semilogx(f, 20*log10(abs(vc_f)), 'g');
+semilogx(f, 20*log10(abs(vs_f)), 'b');
+semilogx(f, 20*log10(abs(V6_f)), 'r');
+xlabel ("f (Hz)");
+ylabel ("Voltage dB");
+ylim([-10 8])
+title ("Frequency Response")
+print ("alinea61.eps", "-depsc");
+
+figure
+semilogx(f, 180/pi*angle(vc_f), 'g', f, 180/pi*angle(vs_f), 'b', f, 180/pi*angle(V6_f), 'r');
+xlabel ("f (Hz)");
+ylabel ("Phase (degrees)");
+title ("Frequency Response")
+print ("alinea62.eps", "-depsc");
