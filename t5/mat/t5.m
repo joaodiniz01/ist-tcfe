39a28879ae08e5631d699a0163b4856632d8a895
joaodiniz01 <joao.diniz@tecnico.ulisboa.pt>
2021-06-08 19:16:17 +0100

    final hopefully

--- t5/mat/t5.m
+++ t5/mat/t5.m
@@ -1,33 +1,41 @@
 %frequency response
 
-R1=1/(1/1000+1/1000); # 2*1kHz //
-R2=1/(1/10000+1/10000+1/10000)
-R3=100000
+R1=1000; 
+R2=1000
+R3=120000
 R4=1000
-C1= 1/(1/(220e-9)+1/(220e-9)+1/(10E-6))
-C2= 1/(1/220E-9+1/(10e-6)+1/(10e-6))
+C1= 220e-9
+C2= 1/(1/220e-9 + 1/220e-9)
 
 f = logspace(1, 8, 70);
 s = i*2*pi*f;
 Ts = ((R1.*C1.*s)./(1+R1.*C1.*s)).*(1+R3./R4).*(1./(1+R2.*C2.*s));
 
 figure;
-semilogx(f, 20*log(abs(Ts)), 'g');
+semilogx(f, 20*log10(abs(Ts)), 'g');
 xlabel ("f (Hz)");
 ylabel ("Frequency Response dB");
 title ("Frequency Response")
 print ("fresponse1.eps");
 
+figure;
+semilogx(f, angle(Ts), 'g');
+xlabel ("f (Hz)");
+ylabel ("Frequency Response Phase (degrees)");
+title ("Frequency Response")
+print ("fresponse2.eps");
+
+
 %central frequency
-fLow = 1/(R1*C1*2*pi);
-fHigh = 1/(R2*C2*2*pi);
+fLow = 1/(R1*C1*2*pi)
+fHigh = 1/(R2*C2*2*pi)
 
 fr = sqrt(fLow*fHigh)
 
 %gain for fr
 s = i*2*pi*fr;
 Tsr = ((R1*C1*s)/(1+R1*C1*s))*(1+R3/R4)*(1/(1+R2*C2*s));
-gain = 20*log(abs(Tsr))
+gain = 20*log10(abs(Tsr))
 
 %Impedance for fr
 ZC1 = 1/(i*2*pi*fr*C1);
@@ -44,8 +52,8 @@ ZO = (ZC2*ZR2)/(ZC2+ZR2)
 fid = fopen ("docImp.tex", "w");
 fprintf(fid, "Central Frequency & %e \\\\ \\hline \n", fr);
 fprintf(fid, "Voltage Gain dB & %e \\\\ \\hline \n", gain);
-fprintf(fid, "Input Impedance & %e \\\\ \\hline \n", ZI);
-fprintf(fid, "Output Impedance & %e \\\\ \n", ZO);
+fprintf(fid, "Input Impedance & %e + %e i\\\\ \\hline \n", real(ZI), imag(ZI));
+fprintf(fid, "Output Impedance & %e + %e i\\\\ \n", real(ZO), imag(ZO));
 fclose (fid);
 
 fid = fopen ("docImp2.tex", "w");
@@ -54,6 +62,6 @@ fprintf(fid, "Voltage Gain dB & %e \\\\ \n", gain);
 fclose (fid);
 
 fid = fopen ("docImp3.tex", "w");
-fprintf(fid, "Input Impedance & %e \\\\ \\hline \n", ZI);
-fprintf(fid, "Output Impedance & %e \\\\ \n", ZO);
+fprintf(fid, "Input Impedance & %e + %e i\\\\ \\hline \n", real(ZI), imag(ZI));
+fprintf(fid, "Output Impedance & %e + %e i\\\\ \n", real(ZO), imag(ZO));
 fclose (fid);
